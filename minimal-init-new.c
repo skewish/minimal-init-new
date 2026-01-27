cd442f459e01c1703f6e3c610d8b3f910e78cf6d
skewish <69397876+skewish@users.noreply.github.com>
2026-01-27 06:30:02 -0800

    Add files via upload

--- minimal-init-new.c
+++ minimal-init-new.c
@@ -7,15 +7,14 @@ int main()
 {
 if(getpid() !=1) return(1);
 
-sigset_t mask;
-sigfillset(&mask);
-sigprocmask(SIG_BLOCK, &mask, 0);
+sigset_t set;
+sigfillset(&set);
+sigprocmask(SIG_BLOCK, &set, 0);
 
 pid_t pid;
 posix_spawn_file_actions_t acts;
 posix_spawnattr_t attr;
-char *argv[]={"rcS", 0};
-char *envp[]={0};
+char *argv[]={"rcS", 0}; char *envp[]={0};
 
 posix_spawn(&pid, "/etc/init.d/rcS", &acts, &attr, argv, envp);
 while(wait(0) !=-1);
