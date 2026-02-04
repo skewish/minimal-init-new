70423de6b86a99028539ce2572c16a5d4238954b
skewish <69397876+skewish@users.noreply.github.com>
2026-02-03 21:12:56 -0800

    Update minimal-init-new.c

--- minimal-init-new.c
+++ minimal-init-new.c
@@ -12,10 +12,10 @@ sigfillset(&set);
 sigprocmask(SIG_BLOCK, &set, 0);
 
 pid_t pid;
-posix_spawn_file_actions_t acts;
-posix_spawnattr_t attr;
+posix_spawn_file_actions_t actions;
+posix_spawnattr_t attrp;
 char *argv[]={"rcS", 0}; char *envp[]={0};
 
-posix_spawn(&pid, "/etc/init.d/rcS", &acts, &attr, argv, envp);
+posix_spawn(&pid, "/etc/init.d/rcS", &actions, &attrp, argv, envp);
 while(wait(0) !=-1);
 }
